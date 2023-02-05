4ec6aa5f84932c671e2513fa6a9fb61fa787dff4
Petr Ptacek <petr.ptacek@olc.cz>
2023-02-05 20:07:40 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RootComponent',
+  data() {
+    return {
+      state: {
+        counter: 0
+      }
+    };
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Counter {{ state.counter }}</h1>
+  </div>
+</template>
